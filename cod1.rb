a219e1be4ac54a3dfe693645569d07b2064936af
Qbs <airborn15@gmail.com>
2020-10-18 21:59:15 +0200

    initial commit. ruby basics

---
+++ cod1.rb
@@ -0,0 +1,52 @@
+11.times {puts"papapa"}
+
+#define two variables
+
+a_variable = 10
+
+str_var = 'blabla'
+
+
+c = [1,2,3,4]
+
+c.each_with_index {|elem,i| c[i]+=1}
+
+# c.each_index {|i| c[i]+=1} 
+
+# for i in 0..c.length-1
+#      c[i]+=1
+#      puts c[i]
+# end
+
+
+
+
+# for i in 0..c.length-1
+#     c[i]=Time.new()
+#     puts c[i]
+#     sleep 1
+# end 
+
+
+
+# while Time.new().to_i % 10 != 0
+#     print Time.new(),"\n"
+#     sleep 1
+# end
+
+
+# t = (0..100).to_a
+
+# for i in 0..t.length-1
+#     e = Time.new().to_i
+#     while e % 10 != 0
+#         print Time.new(),"\n"
+#         sleep 1
+#         e = Time.new().to_i
+#     end
+# end
+
+
+
+
+
